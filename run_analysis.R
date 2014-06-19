747c38ef1aa3c37e4db5814a68be2851a4ee222e
sean <smulready@gmail.com>
2014-06-19 09:51:44 -0400

    Updated method to create output data frame

--- run_analysis.R
+++ run_analysis.R
@@ -1,33 +1,62 @@
 ## Run Analysis
 
 run_analysis <- function () {
-  # Train Data
-  subject_test <- read.table("data/UCI HAR Dataset/train/subject_train.txt")
-  x_train <- read.table("data/UCI HAR Dataset/train/X_train.txt")
-  y_train <- read.table("data/UCI HAR Dataset/train/y_train.txt")
-  body_acc_x_train <- read.table("data/UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt")
-  body_acc_y_train <- read.table("data/UCI HAR Dataset/train/Inertial Signals/body_acc_y_train.txt")
-  body_acc_z_train <- read.table("data/UCI HAR Dataset/train/Inertial Signals/body_acc_z_train.txt")
-  body_gyro_x_train <- read.table("data/UCI HAR Dataset/train/Inertial Signals/body_gyro_x_train.txt")
-  body_gyro_y_train <- read.table("data/UCI HAR Dataset/train/Inertial Signals/body_gyro_y_train.txt")
-  body_gyro_z_train <- read.table("data/UCI HAR Dataset/train/Inertial Signals/body_gyro_z_train.txt")
-  total_acc_x_train <- read.table("data/UCI HAR Dataset/train/Inertial Signals/total_acc_x_train.txt")
-  total_acc_y_train <- read.table("data/UCI HAR Dataset/train/Inertial Signals/total_acc_y_train.txt")
-  total_acc_z_train <- read.table("data/UCI HAR Dataset/train/Inertial Signals/total_acc_z_train.txt")
   
-  # Test Data
-  subject_test <- read.table("data/UCI HAR Dataset/train/subject_test.txt")
-  x_test <- read.table("data/UCI HAR Dataset/test/X_test.txt")
-  y_test <- read.table("data/UCI HAR Dataset/test/y_test.txt")
-  body_acc_x_test <- read.table("data/UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt")
-  body_acc_y_test <- read.table("data/UCI HAR Dataset/test/Inertial Signals/body_acc_y_test.txt")
-  body_acc_z_test <- read.table("data/UCI HAR Dataset/test/Inertial Signals/body_acc_z_test.txt")
-  body_gyro_x_test <- read.table("data/UCI HAR Dataset/test/Inertial Signals/body_gyro_x_test.txt")
-  body_gyro_y_test <- read.table("data/UCI HAR Dataset/test/Inertial Signals/body_gyro_y_test.txt")
-  body_gyro_z_test <- read.table("data/UCI HAR Dataset/test/Inertial Signals/body_gyro_z_test.txt")
-  total_acc_x_test <- read.table("data/UCI HAR Dataset/test/Inertial Signals/total_acc_x_test.txt")
-  total_acc_y_test <- read.table("data/UCI HAR Dataset/test/Inertial Signals/total_acc_y_test.txt")
-  total_acc_z_test <- read.table("data/UCI HAR Dataset/test/Inertial Signals/total_acc_z_test.txt")
+  ## ACTIVITIES ##
+  # Import the activity labels, activity train data, and activity test data
+  act_labels <- read.table("data/UCI HAR Dataset/activity_labels.txt")[,2]
+  act_train <- read.table("data/UCI HAR Dataset/train/y_train.txt")[,1]
+  act_test <- read.table("data/UCI HAR Dataset/test/y_test.txt")[,1]
   
+  # Combine train and test data into single integer vector of activities
+  act_indices <- c(act_train,act_test)
   
+  # Create new factor vector of activities by indexing into labels vector with
+  # integer vector 
+  acts <- act_labels[act_indices]
+  
+  # Null out any data no longer needed (freeing up memory)
+  act_labels <- act_train <- act_test <- act_indices <- NULL
+  
+  ## SUBJECTS ##
+  # Import subject train and test data as separacter integer vectors
+  subject_train <- read.table("data/UCI HAR Dataset/train/subject_train.txt")[,1]
+  subject_test <- read.table("data/UCI HAR Dataset/test/subject_test.txt")[,1]
+  
+  # Combine train and test data into single integer vector of subjects
+  subjects <- c(subject_train,subject_test)
+  
+  # Null out any data no longer needed (freeing up memory)
+  subject_train <- subject_test <- NULL
+  
+  ## FEATURES ##
+  # Import features and assign to character vector
+  features <- read.table("data/UCI HAR Dataset/features.txt", 
+                         stringsAsFactors=FALSE)[,2]
+  
+  # Find features containing a mean or standard deviation and save index to
+  # an integer vector
+  # Note: these indices indicate the columns of remaining data to be imported
+  ind <- grep("([Mm]ean|[Ss]td)",features)
+ 
+  # Column names determined by indexing into 
+  col_names <- features[ind]
+  
+  # Columns to import for x data
+  col_classes <- rep("NULL",561)
+  col_classes[ind] <- "numeric"
+ 
+  ## X DATA ##
+  x_train <- read.table("data/UCI HAR Dataset/train/X_train.txt",
+                        colClasses = col_classes)
+  x_test <- read.table("data/UCI HAR Dataset/test/X_test.txt",
+                       colClasses = col_classes)
+  x <- rbind(x_train,x_test)
+  colnames(x) <- col_names
+  x_train <- x_test <- NULL
+  
+  ## OUTPUT ##
+  full_df <- data.frame(subject = subjects,activity = acts,x)
+  
+  mean_cols
 }
